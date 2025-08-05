6fb9801c84e65d1024ca9049e6de491214cdc5a3
TheInfamousTheta <g.kaustubh@iitg.ac.in>
2025-08-05 22:01:00 +0530

    mess preference screen added

--- frontend2/lib/screens/mess_preference.dart
+++ frontend2/lib/screens/mess_preference.dart
@@ -246,9 +246,62 @@ class _MessChangePreferenceScreenState
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white,
+<<<<<<< Updated upstream
         appBar: AppBar(
           backgroundColor: Colors.white,
           leading: const BackButton(),
+=======
+        leading: const BackButton(),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 0),
+        child: ListView(
+          children: const [
+            Text(
+              "Mess Preference",
+              style: TextStyle(
+                  fontFamily: 'OpenSans_Bold',
+                  color: Themes.feedbackColor,
+                  fontSize: 32,
+                  fontWeight: FontWeight.w700),
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            Text(
+              'Choose the mess that suits your taste or convenience.',
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Text(
+              '1st preference',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            MessChangePrefs(),
+            Text(
+              '2nd preference',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            MessChangePrefs(),
+          ],
+>>>>>>> Stashed changes
         ),
         body: SafeArea(
           child: Padding(
